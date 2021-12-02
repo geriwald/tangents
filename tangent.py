90720468fe41b6e53aae0a0df14a4349ae9c0411
Geriwald <geraud.benazet@gmail.com>
2021-12-02 15:42:28 +0100

    more finer tangents

--- tangent.py
+++ tangent.py
@@ -5,7 +5,7 @@ l = 4*np.pi
 x = np.linspace(-l, l, 1000)
 e = np.linspace(-l, l, 2)
 dx = x[1]-x[0]
-y = np.cos(x) + np.sin(2*x)
+y = x**4 + x**3 - 2 * x**2
 d = np.gradient(y, dx)
 
 fig = plt.figure(figsize=(15, 15))
@@ -14,9 +14,11 @@ for point in zip(x, y, d):
     X, Y, A = point
     B = Y - A * X
     tangent = A * e + B
-    plt.plot(e, tangent, color='k', linewidth=.1)
+    plt.plot(e, tangent,  color='k', linewidth=.1)
 
-plt.plot(x, y, color='r', linewidth=2)
-plt.ylim([-10, 10])
-plt.xlim([-4*np.pi, 4*np.pi])
+
+#plt.plot(x, y, color='r', linewidth=2)
+plt.xlim([-5, 5])
+plt.ylim([-5, 5])
+plt.savefig('poly.png')
 plt.show()
