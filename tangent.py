38c16720735c54723cfa5483ba11b9e4a6dc9b15
Geriwald <geraud.benazet@gmail.com>
2021-12-04 12:35:16 +0100

    new widescreen test

--- tangent.py
+++ tangent.py
@@ -1,24 +1,27 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-l = 4*np.pi
+l = 10*np.pi
 x = np.linspace(-l, l, 1000)
 e = np.linspace(-l, l, 2)
 dx = x[1]-x[0]
-y = np.exp(-abs(x))*np.sin(x)*8
+y = np.cos(x)/x
+
 d = np.gradient(y, dx)
 
-fig = plt.figure(figsize=(15, 15))
+fig = plt.figure(figsize=(70, 15))
+ax = plt.axes()
+ax.set_facecolor('k')
 
 for point in zip(x, y, d):
     X, Y, A = point
     B = Y - A * X
     tangent = A * e + B
-    plt.plot(e, tangent,  color='k', linewidth=.1)
+    plt.plot(e, tangent, color='dimgray', linewidth=.05)
 
 
 #plt.plot(x, y, color='r', linewidth=2)
-plt.xlim([-5, 5])
-plt.ylim([-5, 5])
-plt.savefig('exp_sin.png')
+plt.xlim([-l, l])
+plt.ylim([-2, 2])
+plt.savefig('screen.png')
 plt.show()
